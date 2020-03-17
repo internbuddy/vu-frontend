1e7896aa1e55423cff8c0fb738a1670dbd04394c
Intern Buddy <60249122+internbuddy@users.noreply.github.com>
2020-03-18 00:04:55 +0530

    Update connection.php

--- frontend/connection.php
+++ frontend/connection.php
@@ -1,24 +1,24 @@
 <?php 
 // Local server (Comment the Cloud DB section to work on Local DB)
 // Please comment the above lines before checkin.
-$conn=mysqli_connect('localhost','root','');
-mysqli_select_db($conn,'virtual-university');
-if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
-}
+//$conn=mysqli_connect('localhost','root','');
+//mysqli_select_db($conn,'virtual-university');
+//if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+//}
 
 //cloud database
-// $conn = mysqli_init();
-//mysqli_options ($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
-//$conn->ssl_set(NULL, NULL, '/var/tmp/rds-ca-2019-root.pem', NULL, NULL);
-// $db = mysqli_real_connect ($conn, 'k3beta.c7lkgbzlct6d.ap-south-1.rds.amazonaws.com', 'root', 'maV01X615', 'k3beta', 3306, NULL, MYSQLI_CLIENT_SSL);
-// if (!$db)
-// {
-// 	//echo "error in connection'";
-//     die ('Connect error (' . mysqli_connect_errno() . '): ' . mysqli_connect_error() . "\n");
-// }
-//  else {	
-// $db_name='k3beta';
+$conn = mysqli_init();
+mysqli_options ($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+$conn->ssl_set(NULL, NULL, '/var/tmp/rds-ca-2019-root.pem', NULL, NULL);
+$db = mysqli_real_connect ($conn, 'k3beta.c7lkgbzlct6d.ap-south-1.rds.amazonaws.com', 'root', 'maV01X615', 'k3beta', 3306, NULL, MYSQLI_CLIENT_SSL);
+if (!$db)
+{
+//echo "error in connection'";
+    die ('Connect error (' . mysqli_connect_errno() . '): ' . mysqli_connect_error() . "\n");
+}
+else {	
+$db_name='k3beta';
 $app_name="Virtual University";
 $app_version="1.0";
 $app_logo='<img src="admin/images/logo.png" width="158px" class="brand-logo">';
@@ -29,6 +29,6 @@ $footer="<a href='http://www.mavoix.in' target='_blank' class='text-bold-800 gre
 $project_name="Virtual University";
 $footer_content="Designed & Developed By:";
 $developed_by="<a href='http://www.mavoix.in' target='_blank' class='text-bold-800 grey darken-2'>Mavoix Solutions Pvt Ltd</a>";
-//}
+}
 ?>
 
