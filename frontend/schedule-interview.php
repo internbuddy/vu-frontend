d77a5a9dc33d0e2eb4593bd9bf05177b34eab5db
Intern Buddy <60249122+internbuddy@users.noreply.github.com>
2020-03-20 00:52:54 +0530

    Update schedule-interview.php

--- frontend/schedule-interview.php
+++ frontend/schedule-interview.php
@@ -43,15 +43,33 @@ $url= "https://mavoix.in/virtual-university/api/interview-request.php";
   foreach($data as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
 rtrim($fields_string, '&');
 
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $fields_string);
-curl_setopt($curl,CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+$req = curl_init($url);
+curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($req, CURLOPT_POST, true);
+curl_setopt($req, CURLOPT_POSTFIELDS, $fields_string);
+curl_setopt($req,CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($req, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
 
-$response = curl_exec($curl);
-curl_close($curl);
+$response = curl_exec($req);
+/**
+ * Check if an error is an SSL failure and retry with bundled CA certs on
+ * the assumption that local server has none configured for ext/curl.
+ * Error 77 refers to CURLE_SSL_CACERT_BADFILE which is not defined as
+ * as a constant in PHP's manual for some reason.
+ */
+$error = curl_errno($req);
+if ($error == CURLE_SSL_PEER_CERTIFICATE || $error == CURLE_SSL_CACERT
+|| $error == 77) {
+    curl_setopt($req, CURLOPT_CAINFO, __DIR__ . '/cert-bundle.crt');
+    $response = curl_exec($req);
+}
+
+/**
+ * Any subsequent errors cannot be recovered from while remaining
+ * secure. So do NOT be tempted to disable SSL and try again ;).
+ */
+
+curl_close($req);
 
 $array = json_decode($response, true);
 
