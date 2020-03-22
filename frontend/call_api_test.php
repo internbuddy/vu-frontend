1c716b49a6dcaa378ded455ae1c70482744bab3b
Intern Buddy <60249122+internbuddy@users.noreply.github.com>
2020-03-22 11:10:23 +0530

    Update call_api_test.php

--- frontend/call_api_test.php
+++ frontend/call_api_test.php
@@ -1,5 +1,10 @@
 <?php
-echo "Calling API....";
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 //$url= "https://mavoix.in/virtual-university/api/api-request-test.php";
 //$url= "https://mavoix.in/virtual-university/api/get-student-details.php;
 $url= "https://mavoix.in/virtual-university/api/get-student-info.php;
